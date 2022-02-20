d26bb3b96f602702f5e9ba217f51067f25f060ab
Rebecca Waterson <rebeccawaterson@macbook-pro.home>
2022-02-20 17:10:02 +0000

    added register component

--- src/components/nav bar/navbar.js
+++ src/components/nav bar/navbar.js
@@ -13,14 +13,14 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
       return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+          <button onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</button>
         </nav>
       );
     } else {
       return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-          <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+          <button onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</button>
+          <button onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</button>
         </nav>
       )
     }
